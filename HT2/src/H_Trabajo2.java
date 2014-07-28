b7de6cf0fb1c02f38e7ba7bd5336ecb443138358
Sanchez Tortola <Sanchez Tortola@Meeehhh>
2014-07-27 22:30:03 -0600

    Programa principal

---
+++ HT2/src/H_Trabajo2.java
@@ -0,0 +1,12 @@
+/*Algoritmos y Estructura de Datos
+  Integrantes: Olga Cobaquil  13020
+               Álvaro Sánchez 13657  
+  Fecha: 24/07/14
+  Descripción: Programa Principal de la Hoja de Trabajo*/
+
+/**
+ *
+ * @author Olga Cobaquil
+ */
+
+
